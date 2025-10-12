68293e729f8259a3f728a0cc7006ada99ffc1963
xenabru04 <xena.brusselmans@student.odisee.be>
2025-10-12 12:08:28 +0200

    ConsoleBmi✅

---
+++ IIP1.03.Berekeningen/ConsoleBmi/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Template
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+        Console.WriteLine(@" 
+BMI CALCULATOR
+==============");
+		int lengte = 178;
+		int gewicht = 65;
+		double lengteMeter = Convert.ToDouble(lengte) / 100;
+		double quotient = gewicht / Math.Pow(lengteMeter, 2);
+		quotient = Math.Round(quotient, 1);
+		Console.WriteLine("Lengte (in cm): " + lengte);
+		Console.WriteLine("Gewicht (in kg): " + gewicht);
+		Console.WriteLine("Je BMI bedraagt: " + quotient);
+      }
+   }
+}
