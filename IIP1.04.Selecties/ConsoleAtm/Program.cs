def48ff48534ec4da183eef89640fc72979d8ecb
xenabru04 <xena.brusselmans@student.odisee.be>
2025-10-22 22:24:35 +0200

    ConsoleAtm✅

--- IIP1.04.Selecties/ConsoleAtm/Program.cs
+++ IIP1.04.Selecties/ConsoleAtm/Program.cs
@@ -4,7 +4,6 @@ namespace ConsoleAtm
 {
    class Program
    {
-	  static readonly CultureInfo BE = new CultureInfo("nl-BE");
       static void Main(string[] args)
       {
         string header = @"
@@ -12,20 +11,66 @@ Bankautomaat
 ------------";
        
 	   const int MAX_AFHALING = 600;
-	   const decimal START_SALDO = 1000;
-	   
-	   Console.OutputEncoding = System.Text.Encoding.UTF8;
-	   Console.WriteLine(hearder);
-	   Console.WriteLine($"huidige saldo: {saldo.ToString("C", BE)}");
-	   Console.WriteLine();
-	   
+	   int saldo = 1000;
+	    
+	  Console.WriteLine(header);
+	  Console.WriteLine($"huidige saldo: {saldo}");
+	  Console.WriteLine();
 	  Console.WriteLine("a. afhaling");
 	  Console.WriteLine("b. storting");
 	  Console.WriteLine("c. stoppen");
 	  Console.WriteLine();
 	  
 	  Console.Write("je keuze: ");
-	  char keuze = 
+	  char keuze = Console.ReadKey(true).KeyChar;
+	  Console.WriteLine();
+	  
+	  if (keuze == 'a')
+	  {
+	  
+		  Console.WriteLine("Welk bedrag wil je afhalen: ");
+		  string invoer = Console.ReadLine();
+		  int bedrag = Convert.ToInt32(invoer);
+		  
+		  if (bedrag <=0)
+		  {
+			   Console.WriteLine("Fout: bedrag moet positief zijn");
+		  }
+		  else if (bedrag > MAX_AFHALING || saldo - bedrag < 0)
+		  {
+			   Console.WriteLine("Fout: je kan maximaal {MAX_AFHALEN} afhalen of je saldo is te laag");
+		  }
+		  else if (bedrag % 10 != 0 || bedrag == 10 || bedrag == 30)
+		  { 
+				Console.WriteLine("Fout: enkel briefjes van 20 en 50 zijn mogelijk"); 
+		  }
+		  else
+		  {
+				saldo -= bedrag;
+				Console.WriteLine($"Afhalen ok - het nieuw saldo is € {saldo}");
+		  }
+		}
+        else if (keuze =='b')
+        {    
+		   Console.Write("Welke bedrag wil je storten: ");
+		   string invoer = Console.ReadLine();
+           int stort = Convert.ToInt32(invoer);
+           
+           saldo += stort;
+           Console.WriteLine($"Storting ok - het nieuw saldo is € {saldo}");
+        }
+        else if (keuze == 'c')
+        {
+            Console.WriteLine("Bedankt en tot ziens!");
+        }
+        else
+        {
+             Console.WriteLine("Ongeldige keuze");
+
+	     }
+       
+	  
       }
+	  
    }
 }
